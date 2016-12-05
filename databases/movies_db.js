ed607ae61182fa4ef0a66a497d257991d4decdff
Kyle Siler <ksiler@Kyles-MacBook-Pro.local>
2016-12-04 17:59:48 -0800

    UPdate movies endpoint to include links to actors

---
+++ databases/movies_db.js
@@ -0,0 +1,48 @@
+var movies = [
+    {   name : 'Oceans Eleven',
+        year : '2001',
+        description : 'Danny Ocean and his eleven accomplices plan to rob three Las Vegas casinos simultaneously.',
+        directors : ['Steven Soderbergh'],
+        writers : ['George Clayton Johnson', 'Jack Golden Russell'],
+        actors : ['George Clooney', 'Brad Pitt', 'Julia Roberts'],
+        tags : ['Crime']
+    },
+    { 
+        name : 'Guardians of the Galaxy',
+        year : '2014',
+        description : 'A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.',
+        directors : ['James Gunn'],
+        writers : ['James Gunn', 'Nicole Perlman'],
+        actors : ['Chris Pratt', 'Vin Diesel', 'Bradley Cooper'],
+        tags : ['Action']
+    },
+    { 
+        name : 'Galaxy Quest',
+        year : '1999',
+        description : 'The alumni cast of a space opera television series have to play their roles as the real thing when an alien race needs their help.',
+        directors : ['Dean Parisot'],
+        writers : ['David Howard'],
+        actors : ['Tim Allen', 'Sigourney Weaver', 'Alan Rickman'],
+        tags : ['Comedy']
+    },
+    { 
+        name : 'The Santa Clause',
+        year : '1994',
+        description : 'When a man inadvertantly kills Santa on Christmas Eve, he finds himself magically recruited to take his place.',
+        directors : ['John Pasquin'],
+        writers : ['Leo Benvenuti', 'Steve Rudnick'],
+        actors : ['Tim Allen', 'Judge Reinhold', 'Wendy Crewson'],
+        tags : ['Family', 'Christmas', 'Comedy']
+    },
+    {
+        name : 'Die Hard',
+        year : '1988',
+        description : 'John McClane, officer of the NYPD, tries to save his wife Holly Gennaro and several others that were taken hostage by German terrorist Hans Gruber during a Christmas party at the Nakatomi Plaza in Los Angeles.',
+        directors : ['John McTiernan'],
+        writers : ['Roderick Thorp', 'Jeb Stuart'],
+        actors : ['Bruce Willis', 'Alan Rickman'],
+        tags : ['Action', 'Christmas']
+    }
+]
+
+module.exports = movies;
--- routes/movies.js
+++ routes/movies.js
@@ -1,25 +1,39 @@
 var express = require('express');
 var router = express.Router();
-var movies = [
-    { name : 'Oceans Eleven'},
-    { name : 'Guardians of the Galaxy'},
-    { name : 'Galaxy Quest'},
-    { name : 'Pacific Rim'}
-]
+var movies = require('./../databases/movies_db');
 
 router.get('/', function(req, res) {
-    if(req.query.name) {
-        var result = [];
+    var result = JSON.parse(JSON.stringify(movies));
+    var keys = Object.keys(req.query);
+    console.log(keys);
+
+    if(keys) {
+        result = [];
         movies.forEach(function(value) {
-            if(value.name.includes(req.query.name)) {
-                result.push(value);
+            var match = true;
+            keys.forEach(function(key) {
+                if(!value[key] || !value[key].includes(req.query[key])) {
+                    match = false;
+                }
+            });
+
+            if(match) {
+                var copyOfMovie = JSON.parse(JSON.stringify(value));
+                result.push(copyOfMovie);
             }
         });
-        res.json(result);
-    } else {
-        res.json(movies);
     }
+
+    result.forEach(function (value) {
+        value.actors = buildMovieLink(req, value.name) + '/actors';
+    });
+
+    res.json(result);
 });
+
+var buildMovieLink = function(req, movieName) {
+    return req.protocol + "://" + req.get('host') + req.baseUrl + '/' + encodeURIComponent(movieName);
+}
 
 router.get('/:movie', function(req, res) {
     var result = null;
@@ -28,7 +42,12 @@ router.get('/:movie', function(req, res) {
             result = value;
         }
     });
+    result.actors = buildMovieLink(req, result.name) + '/actors';
     res.json(result);
+});
+
+router.get('/:movie/actors', function(req, res) {
+    res.json({ name : 'Heres an actor' });
 });
 
 module.exports = router
